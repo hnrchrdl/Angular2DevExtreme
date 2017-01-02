6043519c4f787608f2322b5efef8f62883d33b16
hnrchrdl <hinrich@posteo.de>
2017-01-02 22:32:38 +0100

    Sets up Project with angular-cli and adds devextreme and devextreme-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
+import {ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    '../../node_modules/devextreme/dist/css/dx.light.css',
+     '../../node_modules/devextreme/dist/css/dx.common.css',
+     './app.component.css',
+  ],
+  encapsulation: ViewEncapsulation.None // Make these globally available
 })
 export class AppComponent {
   title = 'app works!';
+  arr = [{
+    a: "a",
+    b:"b"
+  },{
+    a: "b",
+    b:"c"
+  }, {v: "v"}];
+
+  test = _.filter(this.arr, {a: "a"});
+
+  okButtonOptions = {
+    text: 'press hard',
+    type: 'normal',
+    onClick: function() {
+        alert('geil');
+    }
+  }
 }
